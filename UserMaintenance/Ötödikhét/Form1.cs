19d1854c7722ca434e7608326a51eb1731e0d378
Dániel Szanto <daniel.szanto@pwc.com>
2022-10-19 19:52:56 +0200

    Api call

--- UserMaintenance/Ötödikhét/Form1.cs
+++ UserMaintenance/Ötödikhét/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ötödikhét.MnbServiceReference;
 
 namespace Ötödikhét
 {
@@ -15,6 +16,19 @@ namespace Ötödikhét
         public Form1()
         {
             InitializeComponent();
+            ApiCall();
+        }
+
+        private void ApiCall()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
 
         private void Form1_Load(object sender, EventArgs e)
