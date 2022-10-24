3d3a08ccfd401007c27b2c5e1e2a950592a87a59
Dániel Szanto <daniel.szanto@pwc.com>
2022-10-24 18:20:07 +0200

    Ball Factory

---
+++ hatodikgyakorlat/hatodikgyakorlat/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatodikgyakorlat.Entities
+{
+    internal class BallFactory
+    {
+        public Ball CreateBall()
+        {
+            return new Ball();
+        }
+
+    }
+}
--- hatodikgyakorlat/hatodikgyakorlat/Form1.cs
+++ hatodikgyakorlat/hatodikgyakorlat/Form1.cs
@@ -16,5 +16,10 @@ namespace hatodikgyakorlat
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
